5b4b6dcbd74929d4b418dab5cfbca7b27a1afef1
Oscar Ryley <oscar.ryley@gmail.com>
2024-03-16 19:56:39 +0000

    Redesigned Task card
    Task card holds all information on both desktop and mobile view, with task name, the length information and a checkbox for whether the task is completed or not.

--- app/client/client.js
+++ app/client/client.js
@@ -4,8 +4,52 @@ window.addEventListener('load', async function (event) {
     this.document.getElementById('container').innerHTML = '';
     for (const index in body) {
         const current = body[index];
-        this.document.getElementById('container').insertAdjacentHTML('afterBegin',
-        `<div class="card song-container my-auto" style="width: 18rem;"><div class="card-body"><h4 class="card-title">${current.name}</h4><p class="card-text"> Length: ${current.length} <br> Completed: ${current.completed}<br> Done?: ${current.done}</div></div> <br><br>`
-        );
+        if (current.done == true){
+            this.document.getElementById('container').insertAdjacentHTML('afterBegin',
+            `            <div id="task-card" class="card border-light" >
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <h5 class="card-title">`+ current.name +`</h5>
+                    </div>
+                    <div class="col">
+                        <p class="card-text" style="display: inline">Completed <input style="width:2rem" value="`+ current.completed +`"></input> out of `+ current.length +` total hours</p>
+                    </div>
+                    <div class="col-md-1">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                            <label class="form-check-label" for="flexCheckChecked">
+                            Done?
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><br>`
+            );
+        } else {
+            this.document.getElementById('container').insertAdjacentHTML('afterBegin',
+            `            <div id="task-card" class="card border-light" >
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <h5 class="card-title">`+ current.name +`</h5>
+                    </div>
+                    <div class="col">
+                        <p class="card-text" style="display: inline">Completed <input style="width:2rem" value="`+ current.completed +`"></input> out of `+ current.length +` total hours</p>
+                    </div>
+                    <div class="col-md-1">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                            <label class="form-check-label" for="flexCheckChecked">
+                            Done?
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><br>`
+            );
         };
+    };
 });

11259eb3a803c8e49e7c8af4787481f28e978f8e
Oscar Ryley <oscar.ryley@gmail.com>
2024-03-16 18:40:58 +0000

    Implemented a GET for all tasks
    SQL database is interacted with via sqlite3 in the server-side backend to SELECT all data from the databse.

--- app/app.js
+++ app/app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const path = require('path');
+const sqlite3 = require('sqlite3');
 const app = express();
 const fs = require('fs');
 app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.json());
+db = new sqlite3.Database("data.db")
+
+let tasks = [];
+
+function sql_all() {
+  db.serialize(() => {
+    db.each("SELECT * FROM tblTask", (err, row) => {
+      tasks.push([row.Name, row.Length, row.Completed, row.Done]);
+    });
+  });
+};
+
+sql_all()
 
 app.get('/', (req, res) => {
   fs.readFile('../client/index.html', function (error, html) {
@@ -15,5 +29,13 @@ app.get('/', (req, res) => {
     res.write(html);
   });
 });
+
+// app.post - POST REQUEST TO ADD TO TABLE
+//db.run("INSERT INTO tblTask (TaskID, Name, Length, Completed, Done) VALUES (?, ?, ?, ?, ?)", [1, "Connect the Frontend and the Backend", 3, 0, false])
+
+app.get('/tasks/all', (req, res) => {
+  res.status(200).send(tasks);
+});
+
 
 module.exports = app;
